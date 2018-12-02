e81481c681fd27be1b083099dceae046c83acbdc
yashkhade <ykhade@nevada.unr.edu>
2018-12-01 23:28:12 -0800

    Solution to Part 2

--- src/com/yashkhade/Question2.java
+++ src/com/yashkhade/Question2.java
@@ -10,8 +10,10 @@ import java.util.Map;
 public class Question2 {
     public static void main(String[] args) throws IOException {
         System.out.println("The value of the checksum is " + solve2A("/Users/yashkhade/IdeaProjects/AdventOfCode2018/src/com/yashkhade/input2.txt"));
+        System.out.println("The same characters found are: " + solve2B("/Users/yashkhade/IdeaProjects/AdventOfCode2018/src/com/yashkhade/input2.txt"));
     }
-//SOLUTION PART1
+
+    //SOLUTION PART1
     public static int solve2A(String inputFileName) throws IOException {
         List<String> lines = Files.readAllLines(Paths.get(inputFileName));
 
@@ -46,7 +48,37 @@ public class Question2 {
     }
 
 
-//SOLUTION PART 2
-    public static
+    //SOLUTION PART 2
+    public static String solve2B(String inputFileName) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get(inputFileName));
+
+        int IDlength = lines.get(0).length();
+        for (int i = 0; i < lines.size(); i++) {
+            for (int j = i + 1; j < lines.size(); j++) {
+                String setA = lines.get(i);
+                String setB = lines.get(j);
+
+                String sameChars = "";
+
+                int noMatch = 0;
+                for (int a = 0; a < setA.length(); a++) {
+                    if (setA.charAt(a) == setB.charAt(a)) {
+                        sameChars += setA.charAt(a);
+                    } else {
+                        noMatch++;
+                        if (noMatch > 1) {
+                            break; // A catch for error, although there should be none.
+                        }
+                    }
+                }
+                if (sameChars.length() == IDlength - 1) {
+                    return sameChars;
+                }
+            }
+        }
+//Just a return
+        return "Error";
+    }
+
 
 }
